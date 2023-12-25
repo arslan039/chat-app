bdf0f7f151abbb4e49b240897fe696c630274f90
arslan039 <105734610+arslan039@users.noreply.github.com>
2023-12-24 22:11:22 -0800

    initial commit

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from "../Components/Navbar.js"
 import Searchbar from "../Components/Searchbar.js"
 import Chats from "../Components/Chats.js"
+
 export default function Sidebar() {
   return (
     <div className='sidebar'>
